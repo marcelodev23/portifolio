fdd66e15bc00d3e4e3dc55ad08917c835b2a45bc
Marcelo Alves <130853760+marcelodev23@users.noreply.github.com>
2023-05-24 15:47:30 +0000

    fdfd

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Marcelo Alves",
+  title: "Desenvolvedor & Estudante",
+  email: "marcelo.dev.alves@gmail.com",
+  gitHub: "marcelodev23",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
